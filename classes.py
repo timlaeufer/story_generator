9b5a97f528971aa6927213415f056d6f82005805
tim-laeufer <laeufertim@gmail.com>
2019-02-25 19:11:09 +0100

    Modified
    Added Descriptions and TODO's to classes.py

--- classes.py
+++ classes.py
@@ -5,6 +5,20 @@ Created on Mon Feb 25 15:45:57 2019
 @author: Tim Laeufer
 
 A collection of classes to be used with the story generator.
+
+TODO:
+    Inner Circle of people,
+    Outer Circle of people,
+    
+    Trait generation
+    
+    Opponent generation
+    
+    Location generation:
+        Name generator
+    
+    Family generation:
+        Name generator
 """
 
 class NPC:
--- constants.py
+++ constants.py
@@ -21,36 +21,33 @@ class Diplomacy:
         return name1 + self.ENEMY_DESC + name2
     
     FRIEND = 'friend'
-    FRIEND_DESC = ' is someone who likes you and is on the side of '
-    def get_friend_desc(self, name1, name2):
-        return name1 + self.FRIEND_DESC + name2
+    FRIEND_DESC = ' is someone who likes and is on the side of '
     
     ALLY = 'ally'
-    ALLY_DESC = 'Ally is someone who will '
-    ALLY_DESC += 'help you in trouble that u have a bond with.'
-    def get_ally_desc(self, name1, name2):
-        return name1 + self.ALLY_DESC + name2
+    ALLY_DESC = ' is an ally of '
     
     OPPONENT = 'opponent'
-    OPPONENT_DESC = 'Opponent is someone that means no harm but has a different.'
-    OPPONENT_DESC += ' interest in one specific field'
-    def get_opponent_desc(self, name1, name2):
-        return name1 + self.OPPONENT_DESC + name2
+    OPPONENT_DESC = ' is against a certain'
+    OPPONENT_DESC += ' interest in one specific field of '
 
 class Feelings:
     #Love
     LOVE = 'love'
-    LOVE_DESC = 'Love is a deep feeling that makes you not see their flaws.'
+    LOVE_DESC = ' loves '
     
     CRUSH = 'crush'
+    CRUSH_DESC = ' has a crush on '
     
     LOW_CRUSH = 'low_crush'
+    LOW_CRUSH_DESC = ' has a small crush on '
 class Family:
     #Family
     FATHER = 'father'
-    FATHER_DESC 
+    FATHER_DESC = ' is the biological father of '
     
     MOTHER = 'mother'
+    MOTHER_DESC = ' is the biological mother of '
+    
     ADOPTIVE_FATHER = 'adoptive_father'
     ADOPTIVE_MOTHER = 'adoptive_mother'
     
